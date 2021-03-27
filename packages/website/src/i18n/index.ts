fc6ef59455a698add7dabf92a7931cb5425bd405
Ryan Elliott-Potter <ryan@ryanep.com>
2021-03-27 22:56:01 +0000

    chore: move translations into i18n folder

--- packages/website/src/i18n/index.ts
+++ packages/website/src/i18n/index.ts
@@ -1,6 +1,6 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { resources } from "#/locales";
+import { resources } from "./translations";
 
 i18n.use(initReactI18next).init({
   resources,
