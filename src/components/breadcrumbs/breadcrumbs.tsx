497e00d01414993b3a553fd6d114d16eabed2e89
Ryan Elliott-Potter <me@ryanep.com>
2024-08-27 18:02:54 +0000

    fix: update breadcrumbs component logic

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -19,33 +19,37 @@ export const Breadcrumbs = ({ breadcrumbs }: BreadcrumbsProps) => {
         Home
       </Link>
 
-      {breadcrumbs.map((breadcrumb, index) => (
-        <Fragment key={breadcrumb.url}>
-          <svg
-            fill="currentColor"
-            height={12}
-            viewBox="0 0 256 512"
-            width={12}
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
-          </svg>
+      {breadcrumbs.map((breadcrumb, index) => {
+        const isLastBreadcrumb = index + 1 < breadcrumbs.length;
 
-          {index === breadcrumbs.length ? (
-            <Link
-              className="text-neutral-700 no-underline hover:opacity-80 dark:text-neutral-300"
-              href={breadcrumb.url}
-              key={breadcrumb.url}
+        return (
+          <Fragment key={breadcrumb.url}>
+            <svg
+              fill="currentColor"
+              height={12}
+              viewBox="0 0 256 512"
+              width={12}
+              xmlns="http://www.w3.org/2000/svg"
             >
-              {breadcrumb.title}
-            </Link>
-          ) : (
-            <span className="text-neutral-700 no-underline dark:text-neutral-300">
-              {breadcrumb.title}
-            </span>
-          )}
-        </Fragment>
-      ))}
+              <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z" />
+            </svg>
+
+            {isLastBreadcrumb ? (
+              <Link
+                className="text-neutral-700 no-underline hover:opacity-80 dark:text-neutral-300"
+                href={breadcrumb.url}
+                key={breadcrumb.url}
+              >
+                {breadcrumb.title}
+              </Link>
+            ) : (
+              <span className="text-neutral-700 no-underline dark:text-neutral-300">
+                {breadcrumb.title}
+              </span>
+            )}
+          </Fragment>
+        );
+      })}
     </nav>
   );
 };
