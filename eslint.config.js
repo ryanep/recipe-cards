10ffc97008de946b206bc53e97deec7a79b9c7e8
Ryan Elliott-Potter <me@ryanep.com>
2025-02-13 18:51:59 +0000

    chore: disable tailwind rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,13 @@
 import { createConfig } from "@ryanep/eslint-config";
 
-export const eslintConfig = createConfig();
+export const eslintConfig = [
+  ...createConfig(),
+  {
+    rules: {
+      // TODO: Re-enable when tailwind plugin supports v4.
+      "tailwindcss/no-custom-classname": "off",
+    },
+  },
+];
 
 export default eslintConfig;
